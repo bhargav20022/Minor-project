4c18604ef5274ede17b03059e19c1ba3907a1b33
Bommana Bhargav <bhargavbommana87@gmail.com>
2026-03-24 12:21:40 +0530

    Update sendEmail.js

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,1 +1,28 @@
 
+const nodemailer = require("nodemailer");
+
+const sendEmail = async (to, subject, htmlContent) => {
+    try {
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.EMAIL_USER, // your email
+                pass: process.env.EMAIL_PASS  // app password
+            }
+        });
+
+        const mailOptions = {
+            from: process.env.EMAIL_USER,
+            to,
+            subject,
+            html: htmlContent
+        };
+
+        await transporter.sendMail(mailOptions);
+        console.log("Email sent successfully!");
+    } catch (err) {
+        console.log("Error sending email:", err);
+    }
+};
+
+module.exports = sendEmail;
