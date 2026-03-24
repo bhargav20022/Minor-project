e63c77c0144b87b977d7685ecf0d62602518d672
Bommana Bhargav <bhargavbommana87@gmail.com>
2026-03-24 11:53:59 +0530

    Update booking.js

--- models/booking.js
+++ models/booking.js
@@ -1,37 +1,27 @@
 const mongoose = require("mongoose");
 
 const bookingSchema = new mongoose.Schema({
-
-listing: {
-
-type: mongoose.Schema.Types.ObjectId,
-
-ref: "Listing"
-
-},
-
-user: {
-
-type: mongoose.Schema.Types.ObjectId,
-
-ref: "User"
-
-},
-
-totalPrice: Number,
-
-paymentId: String,
-
-orderId: String,
-
-status: {
-
-type: String,
-
-default: "Pending"
-
-}
-
+  listing: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Listing"
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User"
+  },
+  checkIn: Date,
+  checkOut: Date,
+  guests: {
+    type: Number,
+    default: 1
+  },
+  totalPrice: Number,
+  paymentId: String,
+  orderId: String,
+  status: {
+    type: String,
+    default: "Pending"
+  }
 });
 
 module.exports = mongoose.model("Booking", bookingSchema);
