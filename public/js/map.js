0e89bff22bb2f8e1747ed39179f833f13f3fc121
Bommana Bhargav <bhargavbommana87@gmail.com>
2026-03-24 18:58:57 +0530

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,16 +1,22 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v12",
+  center: listing.geometry.coordinates,
+  zoom: 9,
+});
 
-    mapboxgl.accessToken = mapToken;
-    const map = new mapboxgl.Map({
-        container: 'map', // container ID
-        center: listing.geometry.coordinates, // starting position [lng, lat]. Note that lat must be set between -90 and 90
-        zoom: 9 // starting zoom
-    });
+const marker = new mapboxgl.Marker({ color: "red" })
+  .setLngLat(listing.geometry.coordinates)
+  .setPopup(
+    new mapboxgl.Popup({ offset: 25 }).setHTML(
+      `<div class="map-click">
+      <h4><b>${listing.title}</b></h4> 
+      <p>Exact loaction will be provided after booking.</p>
+      </div>`
+    )
+  )
+  .addTo(map);
 
-    const marker = new mapboxgl.Marker({color:"red"})
-        .setLngLat(listing.geometry.coordinates) //listing geometry coordinates
-        .setPopup(
-            new mapboxgl.Popup({offset: 25}).setHTML(
-                `<h4>${listing.location}</h4><p>Exact location provided after booking</p>`
-            )
-        )
-        .addTo(map);
+map.addControl(new mapboxgl.ScaleControl());
+map.addControl(new mapboxgl.NavigationControl());
