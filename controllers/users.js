0126e2733083be8aa2fa63cb231f8ab3d7b3e237
Bommana Bhargav <bhargavbommana87@gmail.com>
2026-03-25 08:00:46 +0530

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -1,51 +1,61 @@
-const User=require("../models/user");
+const User = require("../models/user");
+const { sendEmail } = require("../brevo");
 
-module.exports.renderSignupForm=(req, res) => {
+module.exports.renderSignupForm = (req, res) => {
     res.render("users/signup.ejs");
 };
 
-module.exports.signup=async(req, res) => {
+module.exports.signup = async (req, res) => {
     try {
-        let {username, email, password} =req.body;
-    const newUser=new User({email, username});
-    const registeredUser=await User.register(newUser, password);
-    console.log(registeredUser);
-    req.login(registeredUser, (err) => {
-        if (err) {
-            return next(err);
-        }
-         req.flash("success","welcome to wanderlust..");
-    res.redirect("/listings");
-    });
+        let { username, email, password } = req.body;
+        const newUser = new User({ email, username });
+        const registeredUser = await User.register(newUser, password);
+        console.log(registeredUser);
+
+        // ✅ Send welcome email after successful registration
+        await sendEmail({
+            toEmail: email,
+            toName: username,
+            subject: "Welcome to Wanderlust! 🎉",
+            htmlContent: `
+                <h2>Hello ${username}!</h2>
+                <p>Welcome to <strong>Wanderlust</strong> 🌍</p>
+                <p>Your account has been created successfully.</p>
+                <p>Start exploring amazing listings now!</p>
+            `,
+        });
+
+        req.login(registeredUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to Wanderlust..");
+            res.redirect("/listings");
+        });
+
     } catch (e) {
-        req.flash("error",e.message);
+        req.flash("error", e.message);
         res.redirect("/signup");
     }
-  
 };
 
-
-module.exports.renderLoginForm=(req, res) => {
+module.exports.renderLoginForm = (req, res) => {
     res.render("users/login.ejs");
 };
 
+module.exports.login = async (req, res) => {
+    req.flash("success", "Welcome back to Wanderlust!!");
+    const redirectUrl = res.locals.redirectUrl || "/listings";
+    return res.redirect(redirectUrl);
+};
 
-module.exports.login=async (req, res) => {
-        req.flash("success", "welcome back to wanderlust!!");
-
-        const redirectUrl = res.locals.redirectUrl || "/listings";
-         // clear after using
-
-        return res.redirect(redirectUrl);
-    };
-
-
-module.exports.logout=(req, res, next) => {
+module.exports.logout = (req, res, next) => {
     req.logout((err) => {
-        if(err) {
+        if (err) {
             return next(err);
         }
-        req.flash("success","you are logged out!");
+        req.flash("success", "You are logged out!");
         res.redirect("/listings");
     });
 };
+
