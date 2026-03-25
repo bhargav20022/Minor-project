fbb4219025ab40d9e64cad09eb96d54a382a7f3c
Bommana Bhargav <bhargavbommana87@gmail.com>
2026-03-25 12:44:30 +0530

    Update listing.js

--- models/listing.js
+++ models/listing.js
@@ -1,7 +1,6 @@
 const mongoose=require("mongoose");
 const Schema=mongoose.Schema;
 const Review=require("./review.js");
-
 const listingSchema=new Schema({
     title: {
         type: String,
@@ -26,25 +25,19 @@ const listingSchema=new Schema({
         ref: "User",
     },
     geometry:{
-    type: {
-      type: String, // Don't do `{ location: { type: String } }`
-      enum: ['Point'], // 'location.type' must be 'Point'
-      required: true
+        type: {
+          type: String,
+          enum: ['Point'],
+        },
+        coordinates: {
+          type: [Number],
+        },
     },
-    coordinates: {
-      type: [Number],
-      required: true
-    },
-  },
 });
-
-
 listingSchema.post("findOneAndDelete", async (listing) => {
     if (listing) {
         await Review.deleteMany({ _id: { $in: listing.reviews}});
     }
 });
-
-
 const Listing=mongoose.model("Listing",listingSchema);
 module.exports=Listing;
