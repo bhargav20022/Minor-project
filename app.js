812d6c65a9ad6e53e0ae33ad34b8497084bd8c9e
Bommana Bhargav <bhargavbommana87@gmail.com>
2026-04-02 16:27:05 +0530

    Update app.js

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ const sessionOptions = {
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         secure: process.env.NODE_ENV === "production", // ✅ add this
-        sameSite: "strict",   
+        sameSite: "lax",   // ✅ FIXED - allows form submissions
     },
 };
 
