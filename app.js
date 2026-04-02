83ec063d039a0cf556ee44f7257783aa44991392
Bommana Bhargav <bhargavbommana87@gmail.com>
2026-04-02 16:32:01 +0530

    Update app.js

--- app.js
+++ app.js
@@ -76,7 +76,7 @@ const sessionOptions = {
     resave: false,
     saveUninitialized: false,
     cookie: {
-        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+    expires: new Date(Date.now() + 1 * 24 * 60 * 60 * 1000),
         maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
         secure: process.env.NODE_ENV === "production", // ✅ add this
