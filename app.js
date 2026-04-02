fd5c806eb61ae59c97e9e0840525afe7e66cda35
Bommana Bhargav <bhargavbommana87@gmail.com>
2026-04-02 10:59:20 +0530

    Update app.js

--- app.js
+++ app.js
@@ -159,9 +159,9 @@ app.use("/payment", paymentRoute);
 // });
 
 
-app.all("(.*)", (req, res, next) => {
+app.all("/:any", (req, res, next) => {
     next(new ExpressError(404, "Page not found"));
-});;
+});
 
 
 
