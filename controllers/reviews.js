ec43e5227bedcb2999219b008c56dec289fcf4fa
Bommana Bhargav <bhargavbommana87@gmail.com>
2026-05-01 12:33:36 +0530

    Update reviews.js

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,23 +1,33 @@
-const Listing=require("../models/listing");
-const Review=require("../models/review");
+const Listing = require("../models/listing");
+const Review = require("../models/review");
 
+// ==================== CREATE REVIEW ====================
+module.exports.createReview = async (req, res) => {
+  let listing = await Listing.findById(req.params.id);
+  let newReview = new Review(req.body.review);
+  newReview.author = req.user._id;
+  listing.reviews.push(newReview);
+  await newReview.save();
+  await listing.save();
 
-module.exports.createReview=async (req, res) => {
-    let listing=await Listing.findById(req.params.id);
-    let newReview=new Review(req.body.review);
-    newReview.author=req.user._id;
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-    req.flash("success", "Listing Review Created");
+  if (req.headers.accept?.includes("application/json")) {
+    return res.json({ success: true, message: "Review created" });
+  }
 
-    res.redirect(`/listings/${listing._id}`);
+  req.flash("success", "Listing Review Created");
+  res.redirect(`/listings/${listing._id}`);
 };
 
-module.exports.destroyReview=async (req, res) => {
-        let { id, reviewId} = req.params;
-        await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}});
-        await Review.findByIdAndDelete(reviewId);
-        req.flash("success", "Review Deleted");
-        res.redirect(`/listings/${id}`);
-    };
+// ==================== DELETE REVIEW ====================
+module.exports.destroyReview = async (req, res) => {
+  let { id, reviewId } = req.params;
+  await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+  await Review.findByIdAndDelete(reviewId);
+
+  if (req.headers.accept?.includes("application/json")) {
+    return res.json({ success: true, message: "Review deleted" });
+  }
+
+  req.flash("success", "Review Deleted");
+  res.redirect(`/listings/${id}`);
+};
